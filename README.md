32ce3bd468df101b3d70dbfdbd0f15dc3e09fa69
mehmeturmac <mehmeturmac@gmail.com>
2022-07-22 12:10:23 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # spend-money
+
+---
+
+![spend-money](./img/BillGates.png)
+
+---
+
+- Responsive design with [chakra-ui](https://chakra-ui.com/)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+import { Center, Box, Text, Image } from '@chakra-ui/react';
+
+function Header() {
+  const data = useSelector((state) => state.products);
+
+  return (
+    <>
+      <Box mt={5} p={5} bg="white">
+        <Image borderRadius="full" m="auto" src="https://neal.fun/spend/billgates.jpg" alt="Bill Gates" />
+        <Center bg="none">
+          <Text bg="none" mt={5} fontSize={35} fontWeight={700}>
+            Spend Bill Gates' Money
+          </Text>
+        </Center>
+      </Box>
+      <Box bg="green.400" h={50} pos="sticky" top={-1} mt={3} zIndex={1}>
+        <Text bg="green.400" w="100%" textAlign="center" fontSize={40} fontWeight={500}>{`$${new Intl.NumberFormat('en-US').format(data.money)}`}</Text>
+      </Box>
+    </>
+  );
+}
+
+export default Header;
